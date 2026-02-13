3efcab4954b52affbc2a0978c0917e63b29040d4
Alan Reis <alanreisanjo@gmail.com>
2026-02-13 00:19:03 -0300

    refactor: add explicit type to UnpackedPostTagSchema

--- src/domain/schemas/unpacked-post-tag.schema.ts
+++ src/domain/schemas/unpacked-post-tag.schema.ts
@@ -1,6 +1,7 @@
 import { Schema } from "@caffeine/schema";
 import { UnpackedPostTagDTO } from "../dtos";
 
-export const UnpackedPostTagSchema = Schema.make(UnpackedPostTagDTO);
+export const UnpackedPostTagSchema: Schema<typeof UnpackedPostTagDTO> =
+	Schema.make(UnpackedPostTagDTO);
 
 export type UnpackedPostTagSchema = typeof UnpackedPostTagDTO;
